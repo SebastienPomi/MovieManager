86c4140d54afd12f686c3f49a154064fa31a225d
Sébastien Pomi <sebastien.pomi@he-arc.ch>
2016-12-01 09:16:12 +0100

    implémentation des classes métiers et de la classe service

--- src/main/java/ch/hearc/ig/odi/moviemanager/business/Movie.java
+++ src/main/java/ch/hearc/ig/odi/moviemanager/business/Movie.java
@@ -5,10 +5,64 @@
  */
 package ch.hearc.ig.odi.moviemanager.business;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author sebastie.pomi
  */
 public class Movie {
+    
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private List<Person> persons;
+
+    public Movie() {
+        this.persons = new ArrayList<>();
+    }
+
+    public Movie(Long id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.persons = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+    
+    
+    
     
 }
--- src/main/java/ch/hearc/ig/odi/moviemanager/business/Person.java
+++ src/main/java/ch/hearc/ig/odi/moviemanager/business/Person.java
@@ -5,10 +5,75 @@
  */
 package ch.hearc.ig.odi.moviemanager.business;
 
+import ch.hearc.ig.odi.moviemanager.exception.NullParameterException;
+import ch.hearc.ig.odi.moviemanager.exception.UniqueException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author sebastie.pomi
  */
 public class Person {
+    
+    private Long id;
+    private String name;
+    private String producer;
+    private List<Movie> movies;
+
+    public Person() {
+        this.movies = new ArrayList<>();
+    }
+
+    public Person(Long id, String name, String producer) {
+        this.id = id;
+        this.name = name;
+        this.producer = producer;
+        this.movies = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+    
+    public void addMovie(Movie movie) throws UniqueException, NullParameterException {
+        
+    }
+    
+    public void removeMovie(Movie movie){
+        
+    }
+    
+    
+    
+    
     
 }
